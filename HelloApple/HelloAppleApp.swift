36a3c5f91cca13b1fa2ee05b042e266508cf5daf
Jan Dryml <jan.dryml@etnetera.cz>
2023-04-27 11:31:49 +0200

    Initial Commit

---
+++ HelloApple/HelloAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloAppleApp.swift
+//  HelloApple
+//
+//  Created by Jan Dryml on 27.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
